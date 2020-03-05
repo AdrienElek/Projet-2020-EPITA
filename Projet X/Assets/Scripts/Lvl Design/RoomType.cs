505141f0950e3e3696c6beabf5f9aec8acd9e31c
Alexandre GAUTIER <53150686+Aphtasik@users.noreply.github.com>
2020-03-05 17:30:59 +0100

    ajout README et Projet unity

---
+++ Projet X/Assets/Scripts/Lvl Design/RoomType.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = System.Object;
+
+public class RoomType : MonoBehaviour
+{
+    public int type;
+
+    public void RoomDestruction()
+    {
+        Destroy(gameObject);
+    }
+}
