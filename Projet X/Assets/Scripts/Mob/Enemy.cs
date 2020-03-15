b8ae3302e220998ca036f5ce7a30ab97a77ab782
Sprenger07 <leo.ferretti@epita.fr>
2020-03-15 18:09:45 +0100

    Add Statplayer/Mob

---
+++ Projet X/Assets/Scripts/Mob/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public Transform player;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCharacter(movement);
+    }
+
+    void MoveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+}
---
+++ Projet X/Assets/Scripts/Player/PlayerStat.cs
@@ -0,0 +1,249 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : MonoBehaviour
+{
+    private bool isAlive;
+    private int hp;
+    private int maxHp;
+    
+    public int Hp
+    {
+        get { return hp; }
+        set
+        {
+            hp = value;
+            if (hp > maxHp)
+            {
+                hp = maxHp;
+            }
+
+            if (hp < 0)
+            {
+                isAlive = false;
+                hp = 0;
+            }
+        }
+
+    }
+
+    private int torch;
+
+    public int Torch
+    {
+        get => torch;
+        set
+        {
+            torch = value;
+            if (torch < 0)
+            {
+                torch = 0;
+            }
+
+            if (torch > 100)
+            {
+                torch = 100;
+            }
+        }
+    }
+
+    private int stamina;
+    private int maxStamina;
+
+    public int Stamina
+    {
+        get => stamina;
+        set
+        {
+            stamina = value;
+            if (stamina > maxStamina) 
+            {
+                stamina = maxStamina;
+            }
+        }
+    }
+
+    private int money;
+    
+
+    private bool shot;
+    private int shotStamina;
+    public bool Shot
+    {
+        get => shot;
+        set
+        {
+            shot = value;
+            if (stamina >= shotStamina)
+            {
+                stamina -= shotStamina;
+                shot = true;
+            }
+            else
+            {
+                shot = false;
+            }
+        }
+    }
+
+    private int switchWeapon;
+    private int weaponInventoryLength;
+    
+    public int SwitchShot
+    {
+        get => switchWeapon;
+        set
+        {
+            switchWeapon = value;
+            if (switchWeapon < 0)
+            {
+                switchWeapon = switchWeapon % weaponInventoryLength + weaponInventoryLength;
+            }
+            else
+            {
+                switchWeapon %= 3;
+            }
+        }
+    }
+
+
+    public float dashSpeed;
+    private float dashTime;
+    public float starDashTime;
+    private int direction;
+    private int dashStamina;
+
+    private bool dash;
+    public bool Dash
+    {
+        get => dash;
+        set
+        {
+            dash = value;
+            if (stamina >= dashStamina)
+            {
+                stamina -= dashStamina;
+                dash = true;
+            }
+            else
+            {
+                dash = false;
+            }
+        }
+    }
+    
+    
+
+    private bool spell;
+    private int spellStamina;
+    public bool Spell
+    {
+        get => spell;
+        set
+        {
+            spell = value;
+            if (stamina >= spellStamina)
+            {
+                stamina -= spellStamina;
+                spell = true;
+            }
+            else
+            {
+                spell = false;
+            }
+        }
+    }
+
+    private int switchSpell;
+    private int spellInventoryLength;
+
+    public int SwitchSpell
+    {
+        get => switchSpell;
+        set
+        {
+            switchSpell = value;
+            if (switchSpell < 0)
+            {
+                switchSpell = switchSpell % spellInventoryLength + spellInventoryLength;
+            }
+            else
+            {
+                switchSpell %= spellInventoryLength;
+            }    
+        }
+    }
+
+    public int score = 0; //Score
+
+    public void TorchUpdate()
+    {
+        if (Input.GetKey(KeyCode.J))
+        {
+            torch--;
+        }
+        if (Input.GetKey(KeyCode.U))
+        {
+            torch++;
+        }
+    }
+    
+    public bool ShotUpdate()
+    {
+        if (Input.GetKey(KeyCode.J) && shot)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    public bool dashUpdate()
+    {
+        if (Input.GetKey(KeyCode.L) && dash)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    public bool spellUpdate()
+    {
+        if (Input.GetKey(KeyCode.K) && spell)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+        
+    }
+    public void SwitchSpellUpdate()
+    {
+        if (Input.GetKey(KeyCode.O))
+        {
+            switchSpell++;
+        }
+    }
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
