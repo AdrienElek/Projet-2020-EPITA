d4daa8c7274d04862fe8bd5dabee72d8f2ae9314
mathis guilbaud <mathis.guilbaud@epita.fr>
2020-04-21 11:37:58 +0200

    new feature: chest fonctionnel

---
+++ Projet X/Assets/Scripts/items/chest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chest : MonoBehaviour
+{
+	[SerializeField] private GameObject Chest;
+	[SerializeField] private Transform pos;
+	[SerializeField] private GameObject[] obj = new GameObject[2];
+	public int Hp = 3;
+
+	GameObject Content() {
+		int x = Random.Range(0, obj.Length);
+		return obj[x];
+	}
+
+	private void Update()
+	{
+		if (Hp <= 0) {
+			Destroy(Chest);
+			Instantiate(Content(),pos.position,pos.rotation);
+		}
+	}
+}
---
+++ Projet X/Assets/Scripts/items/items.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class items : MonoBehaviour
+{
+	public GameObject objet;
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (Input.GetKeyDown(KeyCode.F)) {
+			Destroy(objet);
+		}
+	}
+
+
+}
