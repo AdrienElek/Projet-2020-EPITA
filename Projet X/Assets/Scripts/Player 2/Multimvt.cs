5a96c3eae8d8738116283b3e2bfe2365f34f0504
mathis guilbaud <mathis.guilbaud@epita.fr>
2020-04-21 18:33:56 +0200

    new feature joueur 2

---
+++ Projet X/Assets/Scripts/Player 2/Multimvt.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Multimvt : MonoBehaviour
+{
+	[SerializeField]private float movespeed = 10f;
+	[SerializeField]private float dashforce = 20f;
+	private Rigidbody2D rb;
+
+	[SerializeField]private GameObject projectile;
+	[SerializeField]private Transform shootpos;
+	[SerializeField]private float shootspeed = 20f;
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	private void Move()
+	{
+		float moverg = Input.GetAxisRaw("Horizontal2");
+		float movehb = Input.GetAxisRaw("Vertical2");
+		transform.Translate(new Vector2(moverg * movespeed * Time.deltaTime,movehb * movespeed * Time.deltaTime));
+	}
+
+	void Shoot() {
+		if (Input.GetKeyDown(KeyCode.M)) {
+
+			GameObject proj = Instantiate(projectile,shootpos);
+			proj.transform.Translate(Vector2.up *  shootspeed );
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		Move();
+    }
+}
