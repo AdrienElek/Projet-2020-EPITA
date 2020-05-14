206df3b9daa63e7b670305298d17ba2c925f9e61
Alexandre GAUTIER <53150686+Aphtasik@users.noreply.github.com>
2020-05-14 16:16:20 +0200

    feat: ajout script music + 2 music (Non libre de droit)

---
+++ Projet X/Assets/Scripts/SoundDesign/BackgroundMusic.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Timeline;
+
+//AUTHOR : Alexandre GAUTIER
+
+public class BackgroundMusic : MonoBehaviour
+{
+    
+    public AudioClip[] backgroundMusicList;
+    public AudioSource source;
+    private int PreviousMusicInt;
+    private float newClip;
+    private float timer;
+
+    void Start()
+    {
+        source = gameObject.AddComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > newClip + 1)
+        {
+            PlayBackgroundMusic();
+            timer = 0;
+        }
+    }
+
+    public void PlayBackgroundMusic()
+    {
+        int rand = Random.Range(0, backgroundMusicList.Length);
+        
+        if (PreviousMusicInt == rand)
+        {
+            while (PreviousMusicInt == rand)
+            {
+                rand = Random.Range(0, backgroundMusicList.Length);
+            }
+        }
+        PreviousMusicInt = rand;
+
+        if (!source.isPlaying)
+        {
+            source.loop = true;
+            source.PlayOneShot(backgroundMusicList[rand]);
+        }
+
+        newClip = backgroundMusicList[rand].length;
+    }
+}
