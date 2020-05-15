328e54979cdc99351b93f25c1bab74d6b2cdf3eb
AdrienElek <adrien.lorge@epita.fr>
2020-05-15 15:43:16 +0100

    ajout scripts inventory et objects

--- Projet X/Assets/Scripts/items/items.cs
+++ Projet X/Assets/Scripts/items/items.cs
@@ -4,16 +4,9 @@ using UnityEngine;
 
 //AUTHOR: mathis GUILBAUD
 
-public class items : MonoBehaviour
+public class Objects : MonoBehaviour
 {
-	public GameObject objet;
-
-	private void OnTriggerStay2D(Collider2D collision)
-	{
-		if (Input.GetKeyDown(KeyCode.F) && collision.tag == "Player") {
-			Destroy(objet);
-		}
-	}
+	
 
 
 }
