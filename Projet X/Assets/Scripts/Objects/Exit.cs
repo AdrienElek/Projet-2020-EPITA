3e0e4da594df6ae2e9d2ac9af4504837fe8d7b73
Alexandre GAUTIER <53150686+Aphtasik@users.noreply.github.com>
2020-05-17 19:50:41 +0200

    feat : script piege a loup (pas entirement fini)

--- Projet X/Assets/Scripts/Objects/Exit.cs
+++ Projet X/Assets/Scripts/Objects/Exit.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//AUTHOR : Alexandre GAUTIER
+
 public class Exit : MonoBehaviour
 {
-    public GameObject exit;
     public GameObject player;
 
     private void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.CompareTag(player.tag))
         {
-            
             Destroy(InstanciateMatrice.boardHolderBlocks.gameObject);
             Destroy(InstanciateMatrice.boardHolderOther.gameObject);
             Destroy(InstanciateMatrice.boardHolderTrap.gameObject);
---
+++ Projet X/Assets/Scripts/Objects/Traps/PiegeALoup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//AUTHOR : Alexandre GAUTIER
+
+public class PiegeALoup : MonoBehaviour
+{
+    public GameObject piegeOuvert;
+    public GameObject piegeFerme;
+    public GameObject player1;
+    public GameObject player2;
+    public AudioSource source;
+    public AudioClip clip;
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.CompareTag(player1.tag))
+        {
+            PlayAudioClip();
+            
+            GameObject piegeALoupFerme = Instantiate(piegeFerme, gameObject.transform.position, Quaternion.identity);
+            piegeALoupFerme.transform.SetParent(InstanciateMatrice.boardHolderTrap);
+            
+            //Destroy(gameObject);
+            
+            //Todo: rajouter les degats au joueur + effet joeuur 2
+        }
+    }
+
+    private void PlayAudioClip()
+    {
+        source = gameObject.GetComponent<AudioSource>();
+        source.volume = 1f;
+        source.PlayOneShot(clip);
+    }
+
+}
