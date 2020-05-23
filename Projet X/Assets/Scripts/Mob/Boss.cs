1316ce481a2c9013b7e25c620791ce06e1dd541d
Alexandre GAUTIER <53150686+Aphtasik@users.noreply.github.com>
2020-05-23 14:51:41 +0200

    fix: Boss slime prefab

---
+++ Projet X/Assets/Scripts/Mob/Boss.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    private void isDead() //TODO : completer qd les slimes pourront mourir
+    {
+        if (gameObject)
+        {
+            Destroy(InstanciateMatrice.boardHolderBlocks.gameObject);
+            Destroy(InstanciateMatrice.boardHolderOther.gameObject);
+            Destroy(InstanciateMatrice.boardHolderTrap.gameObject);
+            Destroy(InstanciateMatrice.boardHolderEntities.gameObject);
+
+            InstanciateMatrice.genLevel = true;
+        }
+    }
+}
