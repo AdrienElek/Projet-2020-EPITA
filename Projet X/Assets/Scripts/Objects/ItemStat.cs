963252b156c8834018ff248baaa6414f3f8a3335
Sprenger07 <leo.ferretti@epita.fr>
2020-05-27 21:04:18 +0200

    fix mob

---
+++ Projet X/Assets/Scripts/Objects/ItemStat.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    //Leo Ferretti
+public class ItemStat : MonoBehaviour
+{
+    public string item;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (item == "bread")
+            {
+                PlayerStat.hp += 5;
+            }
+            if (item == "fish")
+            {
+                PlayerMouvement.bulletSpeed += 0.5f;
+            }
+            if (item == "armor")
+            {
+                PlayerStat.maxHp += 10;
+                PlayerStat.hp += 5;
+
+            }
+            if (item == "boot")
+            {
+                PlayerMouvement.moveSpeed += 0.5f;
+            }
+        }
+    }
+}
